abf141edb56b86abd6ed53dd260b293008eee8d8
DESKTOP-KK99BOF\Welcome <udemystaletale@gmail.com>
2024-04-06 12:09:11 +0530

    Validate responce body

--- src/test/java/GET_TEST.java
+++ src/test/java/GET_TEST.java
@@ -1,33 +1,51 @@
 import static io.restassured.RestAssured.*;
 import static io.restassured.matcher.RestAssuredMatchers.*;
 import static org.hamcrest.Matchers.*;
+
+import org.testng.annotations.Test;
+
 public class GET_TEST {
-public static String BASE_URL ="https://api.postman.com";
-public static String WORKSPACE ="workspaces";
-static int RESP_200 =200;
-static String apiKey_1 ="-65e5ed1b2cbf790001869f42-";
-static String apiKey_2  ="dfea8a7457bed05a72d9d335de0797c27a";
+	public static String BASE_URL = "https://api.postman.com";
+	public static String WORKSPACE = "/workspaces";
+	static int RESP_200 = 200;
+	static String apiKey_1 = "-65e5ed1b2cbf790001869f42-";
+	static String apiKey_2 = "dfea8a7457bed05a72d9d335de0797c27a";
 
 	@org.testng.annotations.Test
 	void Test() {
 		System.out.println("Test is good run");
 
 	}
-@org.testng.annotations.Test
+
+	@org.testng.annotations.Test
 	void get_response() {
 
-		//pre-requists like base url authoriszation and header etc
-		given().baseUri(BASE_URL).header("X-API-Key", "PMAK"+apiKey_1+apiKey_2).
+		// pre-requists like base url authoriszation and header etc
+		given().baseUri(BASE_URL).header("X-API-Key", "PMAK" + apiKey_1 + apiKey_2).
+
+		// when for after specific action done or server let get patch etc
+
+				when().get(WORKSPACE).
+
+				// then --> validate resopnse form server
+
+				then().log().all().assertThat().statusCode(RESP_200);
+	}
+	
+	
+	@org.testng.annotations.Test
+	void validate_responce_body() {
+		given().baseUri(BASE_URL).header("X-API-Key", "PMAK" + apiKey_1 + apiKey_2).	when().get(WORKSPACE).
+		then().log().all().assertThat().statusCode(RESP_200).
 		
+		//using groovy's gpatch
 		
-		//when for after specific action done or server let get patch etc
+		//here single body with two assertions
+		body("workspaces.name",hasItems("Team Workspace", "My Workspace", "w1", "Test Post workspace"),
+			"workspaces.name[1]",is(equalTo("My Workspace"))).
+		//different body
+		body("workspaces.id", hasItem("fe759805-408a-4946-8a40-8dbbca2e3236"));
 		
-		when().get(WORKSPACE).
-		
-		
-		//then --> validate resopnse form server
-		
-		then().log().all().assertThat().statusCode(RESP_200);
 	}
 
 }
