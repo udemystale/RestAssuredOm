b824f74c1c6f197089ddac6e5a86779f8fc6dee9
DESKTOP-KK99BOF\Welcome <udemystaletale@gmail.com>
2024-05-19 21:59:04 +0530

    Multiple query params

--- src/test/java/Section18_Handling_Request_param/A_Single_Query_param.java
+++ src/test/java/Section18_Handling_Request_param/A_Single_Query_param.java
@@ -4,6 +4,8 @@ import static io.restassured.RestAssured.*;
 import static io.restassured.matcher.RestAssuredMatchers.*;
 import static org.hamcrest.Matchers.*;
 
+import java.util.HashMap;
+
 import org.testng.annotations.Test;
 
 public class A_Single_Query_param {
@@ -11,6 +13,23 @@ public class A_Single_Query_param {
 	@Test
 	void single_query_Param() {
 		given().baseUri("https://postman-echo.com/").queryParam("foo1", "book1").log().all().get("get").then().log()
+				.all().assertThat().statusCode(200);
+	}
+	
+	@Test
+	void Multi_query_Param() {
+		
+		HashMap<String,String> queryParam = new  HashMap<String,String>();
+		
+		queryParam.put("q1", "v1");
+		queryParam.put("q2", "v2");
+		queryParam.put("q3", "v4");
+		given().baseUri("https://postman-echo.com/").
+		queryParam("foo1", "book1").
+		queryParam("foo12", "book2").
+		queryParams(queryParam)
+		
+		.log().all().get("get").then().log()
 				.all().assertThat().statusCode(200);
 	}
 }
